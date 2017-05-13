ade7115c2395a138adf1d63a8496074937aed7b2
Corey <cry@Coreys-MacBook-Air.local>
2017-05-13 09:39:00 -0700

    Completed jsoninator and recursion review

--- jsoninator.js
+++ jsoninator.js
@@ -6,9 +6,11 @@ const _ = require('underscore'); // the real one! :)
 
  
 const stringify = function(obj) {
-  let arrOfKeyVals = [];
+  let arrayKeyValues = [];
   let arrayValues = [];
   let objKeys = [];
+
+    // Arrays
 
     if (typeof(obj) === 'number' || typeof(obj) === 'boolean' || obj === null) {
         return '' + obj;
@@ -22,63 +24,24 @@ const stringify = function(obj) {
             return '[' + arrayValues + ']';
         };
     }
-    /*********CHECK FOR OBJECT**********/
-    // else if (obj instanceof Object) {
-    //     //get object keys
-    //     objKeys = Object.keys(obj);
-    //     //set key output;
-    //     objKeys.forEach(function(key) {
-    //         var keyOut = '"' + key + '":';
-    //         var keyValOut = obj[key];
-    //         //skip functions and undefined properties
-    //         if (keyValOut instanceof Function || typeof keyValOut === undefined)
-    //             arrOfKeyVals.push('');
-    //         else if (typeof keyValOut === 'string')
-    //             arrOfKeyVals.push(keyOut + '"' + keyValOut + '"');
-    //         else if (typeof keyValOut === 'boolean' || typeof keValOut === 'number' || keyValOut === null)
-    //             arrOfKeyVals.push(keyOut + keyValOut);
-    //         //check for nested objects, call recursively until no more objects
-    //         else if (keyValOut instanceof Object) {
-    //             arrOfKeyVals.push(keyOut + stringify(keyValOut));
-    //         }
-    //     });
-    //     return '{' + arrOfKeyVals + '}';
-    // }
+    // Objects 
+      else if (typeof(obj) === 'object') {
+        objKeys = Object.keys(obj);
+        _.each(objKeys, (key) => {
+          let suffix = '"' + key + '":';
+          let currentKey = obj[key];
+          if (typeof(currentKey) === 'string') {
+            arrayKeyValues.push(suffix + '"' + currentKey + '"');
+          } else if (typeof(currentKey) === 'object') {
+              arrayKeyValues.push(suffix + stringify(currentKey));
+          }
+        });
+        return '{' + arrayKeyValues + '}';
+      };
 }
 
 
 
-
-//   let arrOfKeyVals = [];
-//   let arrVals = [];
-//   let objKeys = [];
-
-//   if (typeof(obj) === 'number' || typeof obj === 'boolean' || obj === null) {
-//     return '' + obj;
-//   } else if (typeof obj === 'string') {
-//       return '"' + obj + '"';
-//   } else if (Array.isArray(obj) && obj.length === 0) {
-//       return '[]'      
-//   } else if (Array.isArray(obj) && obj.length === 1) {
-//       return '[' + obj[0] + ']';
-//   // } else if (Array.isArray(obj)) {
-//   //     if (obj[0] === undefined) {
-//   //       return '[]';
-//   //   } else {
-//   //       _.each(obj, (val) => arrayValues.push(stringify(val)))
-//   //       return '[' + arrayValues + ']';
-//   //   }
-//   }    else if (Array.isArray(obj)) {
-//         //check for empty array
-//         if (obj[0] === undefined)
-//             return '[]';
-//         else {
-//             obj.forEach(function(el) {
-//                 arrVals.push(stringify(el));
-//             });
-//             return '[' + arrVals + ']';
-//         }
-//     }
 
 
   // your code goes here
